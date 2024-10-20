f54eaa44dd97fd88ad9791a30f156e7f4785699d
SnazzyNivesh522 <niveshpritmani@gmail.com>
2024-10-20 17:10:50 +0530

    updated for deployment on to vercel

--- server/server.js
+++ server/server.js
@@ -30,10 +30,8 @@ mongoose.connect(process.env.MONGO_URI, clientOptions,{
 
 
 // Start the server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-app.listen(() => {
-  console.log('Server running');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
